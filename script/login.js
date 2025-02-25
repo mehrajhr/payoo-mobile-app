d70c57d4a6e813bd233f6fb412922a62e4c5f67c
Mehraj Hasan Riaj <mehrajhasanriajcse@gmail.com>
2025-02-25 20:16:59 +0600

    add login with js

---
+++ script/login.js
@@ -0,0 +1,18 @@
+document.getElementById('login-btn').addEventListener('click',function (event) {
+    event.preventDefault();
+    const mobileNumber = document.getElementById('mobile-number').value;
+    
+    const pin = document.getElementById('pin').value;
+    
+    if(mobileNumber.length === 11){
+        if(pin === '1234'){
+            window.location.href = "./main.html"
+        }
+        else{
+            alert("Please write valid pin")
+        }
+    }
+    else{
+        alert("need valid mobile number");
+    }
+})
